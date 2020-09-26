9a327ca3c0243e187caed795cecbb5ed66367c82
shurasementsov <71932566+shurasementsov@users.noreply.github.com>
2020-09-26 21:12:43 +0300

    Это также как и предыдущее, отдельное решение. Только в отличие от него здесь Python взаимодействует с R

--- main.py
+++ main.py
@@ -1,64 +1,12 @@
-def prepare_data(fetched_info):
-    fetched_info = fetched_info.lstrip()
-    fetched_info = fetched_info.rstrip()
-    fetched_info = fetched_info.replace("\n", "")
-    norm_info = fetched_info.replace("  ", "")
+from rpy2 import robjects #в настройках основного интерпретатора нужно по классике закаать необходимый модуль (он так и называется pry2)
 
-    norm_info = norm_info.lstrip()
-    norm_info = norm_info.rstrip()
-    return norm_info
+def function1(input, output):
+    r = robjects.r
+    r.source("rtest.R")
+    p = r.rtest(input, output)
+    return p
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    from bs4 import BeautifulSoup
-    import requests
+    a = function1(12, 12)
 
-    url = "https://qna.habr.com"
-    page = requests.get(url)
-    print(page.status_code)
-
-    lists = [] #список интересных статей
-    interesting_items = [] #список конкретного элемента
-
-    soup = BeautifulSoup(page.text, "html.parser")
-    #print soup
-
-    # с помощью findAll перечислим все необходимые нам элементы, сохраняя нужный нам тэг с классом
-    lists = soup.findAll('li', class_='content-list__item') #в lists хранятся основные вопросы (их ≈20), а также самые популярные (справа расположенные и они не помечаются тэгами языков программирования или технологий
-
-    for i in range(len(lists)):
-        if lists[i].find('div', class_='question__content') is not None:
-            buf0 = lists[i].findAll('div', class_='question__content')[0].findAll('div', class_='question__content_fluid')[0]
-            buf1 = prepare_data(buf0.findAll('div', class_='question__tags')[0].findAll('ul', class_='tags-list tags-list_short')[0].text)
-            buf2 = prepare_data(buf0.findAll('h2', class_='question__title')[0].text)
-            interesting_items.append([str(i+1), buf1, buf2])
-
-    for i in range(len(interesting_items)):
-        print(interesting_items[i][0] + '. [' + interesting_items[i][1] + '] ' + interesting_items[i][2])
-
-    # ВЫБИРАЕМ ПАПКУ СОХРАНЕНИЕ ЧЕРЕЗ ДИСПЕТЧЕР ФАЙЛОВ
-    import csv
-    from tkinter import filedialog
-    from tkinter import *
-    root = Tk()
-    # так можно открывать расположение файлов: myFileName = filedialog.askopenfilename(initialdir="/", title="Se", filetypes=[["av", "*.csv"], ["a", "*.xlsx"]])
-    #myPathName = filedialog.askdirectory(initialdir="/", title="Select directory")
-    f = filedialog.asksaveasfile(initialdir="/", title="Сохраните файл")
-
-    #при недуачной попытке сохраняется неудачный файл
     None
-
-    start_check = f.name.rfind('/')
-    if start_check == -1:
-        start_check = 0
-    f_name = f.name
-    import re
-    regexp = r'\w.csv$'
-    m = re.search(regexp, f_name)
-    if m is None:
-        f_name += '.csv'
-    if f is not None:
-        with open(f_name, 'w', encoding='utf-8') as file: #придётся дописывать этот костыль, чтобы ему удавалось сохранять информацию в utf-8 (этот метод хорош тем, что стирает старую информацию (передвигая указатель на начало))
-            writer = csv.writer(file, delimiter="\t")
-            writer.writerows(interesting_items)
---
+++ rtest.R
@@ -0,0 +1,7 @@
+rtest = function(input ,output) {
+  a <- input
+  b <- output
+  outpath <- a+b
+  print(a+b)
+  return(outpath)
+}
