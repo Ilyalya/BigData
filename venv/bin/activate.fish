79d4eed64d93844fd6fa0fe5352cf04977d5cc3d
ilya <ilya.lebedev.98@mail.ru>
2020-09-28 20:16:55 +0300

    саня, я всё настроил, и зачем ты удалил старый main пусть будет пока что

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/aleksandrsemencov/Documents/GitHub/BigData/venv"
+set -gx VIRTUAL_ENV "/Users/lebedevila/PycharmProjects/BigData2/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
