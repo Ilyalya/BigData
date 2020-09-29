391cb4d62359c70b7b79cdaacdd43a44a4741424
shurasementsov <71932566+shurasementsov@users.noreply.github.com>
2020-09-29 17:16:48 +0300

    Сперва выскакивало, а потом перестало.

--- main.py
+++ main.py
@@ -1,45 +1,34 @@
 import time
-import concurrent.futures
-from PIL import Image, ImageFilter
-
-img_names = [
-    'photo-1516117172878-fd2c41f4a759.jpg',
-    'photo-1532009324734-20a7a5813719.jpg',
-    'photo-1524429656589-6633a470097c.jpg',
-    'photo-1530224264768-7ff8c1789d79.jpg',
-    'photo-1564135624576-c5c88640f235.jpg',
-    'photo-1541698444083-023c97d3f4b6.jpg',
-    'photo-1522364723953-452d3431c267.jpg',
-    'photo-1513938709626-033611b8cc03.jpg',
-    'photo-1507143550189-fed454f93097.jpg',
-    'photo-1493976040374-85c8e12f0c0e.jpg',
-    'photo-1504198453319-5ce911bafcde.jpg',
-    'photo-1530122037265-a5f1f91d3b99.jpg',
-    'photo-1516972810927-80185027ca84.jpg',
-    'photo-1550439062-609e1531270e.jpg',
-    'photo-1549692520-acc6669e2f0c.jpg'
-]
-
-t1 = time.perf_counter()
-
-size = (1200, 1200)
+import sys
+import os
+from multiprocessing import Process, freeze_support
 
 
-def process_image(img_name):
-    img = Image.open(img_name)
-
-    img = img.filter(ImageFilter.GaussianBlur(15))
-
-    img.thumbnail(size)
-    img.save(f'processed/{img_name}')
-    print(f'{img_name} was processed...')
+def info(title):
+    if hasattr(os, 'getppid'):  # only available on Unix
+        print('{0}:\tPID={1} PPID={2}'.format(title, os.getpid(), os.getppid()))
+    else:
+        print('{0}:\tPID={1}'.format(title, os.getpid()))
 
 
-with concurrent.futures.ProcessPoolExecutor() as executor:
-    executor.map(process_image, img_names)
+def fun(name):
+    info('порождённый процесс')
+    print('процесс {0} выполняет функцию с параметром {1}'.format(os.getpid(), name))
+    time.sleep(0.5)
 
 
-t2 = time.perf_counter()
+if __name__ == '__main__':
+    freeze_support()
+    nproc = len(sys.argv) > 1 and int(sys.argv[1]) or 3
+    print('число дочерних процессов ', nproc)
+    info('родительский процесс')
+    procs = []
+    for i in range(nproc):
+        procs.append(Process(target=fun, args=(i,)))
+    for i in range(nproc):
+        procs[i].start()
+    for i in range(nproc):
+        procs[i].join()
+    print('завершается родительский процесс')
 
-print(f'Finished in {t2-t1} seconds')
-None
+    None
